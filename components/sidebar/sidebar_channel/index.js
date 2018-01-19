010ce4d2354e84ab70f8a79a093d79314d256768
Elias Nahum <nahumhbl@gmail.com>
2018-01-19 13:05:38 -0300

    handle unread mentions with redux

--- components/sidebar/sidebar_channel/index.js
+++ components/sidebar/sidebar_channel/index.js
@@ -3,7 +3,8 @@
 
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {getChannel, getChannelsNameMapInCurrentTeam} from 'mattermost-redux/selectors/entities/channels';
+
+import {getChannel, getChannelsNameMapInCurrentTeam, getMyChannelMember} from 'mattermost-redux/selectors/entities/channels';
 import {getUserIdsInChannels, getUser} from 'mattermost-redux/selectors/entities/users';
 import {get as getPreference} from 'mattermost-redux/selectors/entities/preferences';
 import {savePreferences} from 'mattermost-redux/actions/preferences';
@@ -11,7 +12,6 @@ import {leaveChannel} from 'mattermost-redux/actions/channels';
 import {getConfig} from 'mattermost-redux/selectors/entities/general';
 
 import {Constants} from 'utils/constants.jsx';
-import ChannelStore from 'stores/channel_store.jsx';
 
 import SidebarChannel from './sidebar_channel.jsx';
 
@@ -30,7 +30,13 @@ function mapStateToProps(state, ownProps) {
         }
     }
 
-    const unreads = ChannelStore.getUnreadCount(channel.id);
+    const member = getMyChannelMember(state, ownProps.channelId);
+    const unreadMentions = member ? member.mention_count : 0;
+
+    let unreadMsgs = channel && member ? channel.total_msg_count - member.msg_count : 0;
+    if (unreadMsgs < 0) {
+        unreadMsgs = 0;
+    }
 
     let teammate = null;
     if (channel.teammate_id) {
@@ -51,8 +57,8 @@ function mapStateToProps(state, ownProps) {
         showTutorialTip: tutorialStep === Constants.TutorialSteps.CHANNEL_POPOVER && config.EnableTutorial === 'true',
         townSquareDisplayName: channelsByName[Constants.DEFAULT_CHANNEL] && channelsByName[Constants.DEFAULT_CHANNEL].display_name,
         offTopicDisplayName: channelsByName[Constants.OFFTOPIC_CHANNEL] && channelsByName[Constants.OFFTOPIC_CHANNEL].display_name,
-        unreadMsgs: unreads.msgs,
-        unreadMentions: unreads.mentions,
+        unreadMsgs,
+        unreadMentions,
         membersCount
     };
 }
